8866a206e64abcaa09e255f7edfbce600024d060
galinad <de_galina@abv.bg>
2025-09-18 23:09:34 +0300

    BaseTests

---
+++ scr/test/java/Base/BaseTests.java
@@ -0,0 +1,22 @@
+package Base;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTests {
+
+    private WebDriver driver;
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\galin\\IdeaProjects\\QA-Automation-Test-Automation-University\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().window().setSize(new Dimension(375,812));
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+    public static void main(String []arg){
+        BaseTests test = new BaseTests();
+        test.setUp();
+    }
+}
